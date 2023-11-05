b4d85de0cc2bc0cad2e956b324b27cf29ef5a8d6
YongJunLim <19851315+YongJunLim@users.noreply.github.com>
2023-11-05 16:41:37 +0800

    Refactored puzzle.d.ts and removed redundant la.tsx

--- src/app/puzzles/puzzle.d.ts
+++ src/app/puzzles/puzzle.d.ts
@@ -1,8 +1,8 @@
 export type Puzzle = {
-    name: string;
-    category: string;
-    url_path: string;
-    illustration: string;
-    illustration_alt: string;
-    passcode?: number;
-  };
+  name: string;
+  category: string;
+  url_path: string;
+  illustration: string;
+  illustration_alt: string;
+  passcode?: number;
+};
